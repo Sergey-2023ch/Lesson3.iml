8cc307894262ba91c67ed936d1163ebaa852c113
Sergey-2023ch <639j1mo64aaz@mail.ru>
2023-10-21 13:16:53 +0300

    second commit

--- src/Calculator.java
+++ src/Calculator.java
@@ -5,14 +5,41 @@ public class Calculator {
     public static void main(String[] args) {
 
         Scanner s = new Scanner(System.in);
-        System.out.println("Input a=");
-        int a = s.nextInt();
-        System.out.println("Input b=");
-        double b = s.nextDouble();
-        double sum = a + b;
-        double dif = a - b;
-        double multi = a * b;
-        double div = a / b;
-        System.out.printf("Sum = %f, difference = %f, multiplication = %f, division = %f",sum,dif,multi,div);
+        boolean c = true;
+
+        while (c) {
+            System.out.println("Input a =");
+            int a = s.nextInt();
+            System.out.println("Input b =");
+            double b = s.nextDouble();
+            s.nextLine();
+            System.out.println("Input operation : 1-(+),2-(-),3-(*),4-(/),0-exit");
+            String operation = s.nextLine();
+
+            switch (operation) {
+
+                case "1":
+                    System.out.println("sum a+b = " + (a + (int) b));
+                    break;
+                case "2":
+                    System.out.println("difference a-b = " + (a - (int) b));
+                    break;
+                case "3":
+                    System.out.println("product a*b = " + (a * (int) b));
+                    break;
+                case "4":
+                    System.out.println("division a/b = " + (a / b));
+                    break;
+                case "0":
+                    c = false;
+                    break;
+                default:
+                    System.out.println("You enter wrong operation. Try again.");
+                    break;
+            }
+        }
+
     }
 }
+
+
