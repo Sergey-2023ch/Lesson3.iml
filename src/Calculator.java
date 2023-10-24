616b2e681abfdc5a312da582a93e2ccb9531964d
Sergey-2023ch <639j1mo64aaz@mail.ru>
2023-10-24 19:32:42 +0300

    third commit

--- src/Calculator.java
+++ src/Calculator.java
@@ -8,27 +8,27 @@ public class Calculator {
         boolean c = true;
 
         while (c) {
+
             System.out.println("Input a =");
             int a = s.nextInt();
             System.out.println("Input b =");
-            double b = s.nextDouble();
+            int b = s.nextInt();
             s.nextLine();
             System.out.println("Input operation : 1-(+),2-(-),3-(*),4-(/),0-exit");
             String operation = s.nextLine();
 
             switch (operation) {
-
                 case "1":
-                    System.out.println("sum a+b = " + (a + (int) b));
+                    sum(a, b);
                     break;
                 case "2":
-                    System.out.println("difference a-b = " + (a - (int) b));
+                    dif(a, b);
                     break;
                 case "3":
-                    System.out.println("product a*b = " + (a * (int) b));
+                    multi(a, b);
                     break;
                 case "4":
-                    System.out.println("division a/b = " + (a / b));
+                    div(a, b);
                     break;
                 case "0":
                     c = false;
@@ -38,8 +38,34 @@ public class Calculator {
                     break;
             }
         }
+    }
+
+    public static void sum(int a, int b) {
+        System.out.println("sum a+b = " + (a + b));
+    }
+
+    public static void dif(int a, int b) {
+        System.out.println("difference a-b = " + (a - b));
+    }
+
+    public static void multi(int a, int b) {
+        System.out.println("multiplication a*b = " + (a * b));
+    }
+
+    public static void div(int a, int b) {
+        if (b == 0) {
+            System.out.println("ERROR!!! , b=0");
+        }
+         else {
+            System.out.println("division a/b = " + (a / (double)b));
+        }
 
     }
 }
+
+
+
+
+
 
 
